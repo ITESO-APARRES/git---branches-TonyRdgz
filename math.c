7809cd68df2c1c6253455c4a165f4ab6a13a8fa8
Tony Rdgz <ars.proy@gmail.com>
2022-10-12 17:26:52 -0500

    new fun doble dobleDouble

--- math.c
+++ math.c
@@ -2,9 +2,15 @@
 #include "math.h"
 
 int dobleInt(int a){
-	
 	if(a > MAX_INT){
 		return -1;
+	}
+	return a*2;
+}
+double dobleDouble(double a){
+	if(a > MAX_DOUBLE){
+		return -1;
+		
 	}
 	return a*2;
 }
